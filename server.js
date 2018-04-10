3ba4af637e7be78f489de22a8017f50acb30821c
Tim Hill <peakcodes@gmail.com>
2018-04-10 14:19:38 -0400

    update

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// Serve up static assets
+app.use(express.static("client/build"));
+// Add routes, both API and view
+app.use(routes);
 
 app.get('/', (req, res) => {
   res.send("Hello World!");
 });
+
+const mongoose = require('mongoose');
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/mern",
+  {
+    useMongoClient: true
+  }
+);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
